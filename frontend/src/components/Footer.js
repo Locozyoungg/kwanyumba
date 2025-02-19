a02c5bf7bf4b618bb4bd75ad33751401d6dadd64
Locozyoungg <98713788+Locozyoungg@users.noreply.github.com>
2025-02-20 00:08:35 +0300

    Update Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+// File: src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Kwanyumba. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
